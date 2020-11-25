7558e2a19d7c96730e28f190eb15b3c8387aea72
George Popovic <georgesmpopovic@gmail.com>
2020-11-25 11:54:13 -0500

    First commit. Model (User), Migration (users), Controller (RestApiController), and Routes (see api.php) have been created.

---
+++ database/migrations/2020_11_25_162153_users.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    { // Create users table with the following columns:
+        // username, password, email, firstName, lastName
+        Schema::create('users', function(Blueprint $table){
+            $table->string("username");
+            $table->string("password");
+            $table->string("email");
+            $table->string("firstName");
+            $table->string("lastName");
+            $table->increments("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Schema::drop("users");
+
+        // Drop table users if it already exists.
+        Schema::dropIfExists("users");
+    }
+}
